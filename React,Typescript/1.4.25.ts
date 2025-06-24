1f474f233a73c5a7c776ed35585c55c27ec1bc03
jyothigithub12 <144435403+jyothigithub12@users.noreply.github.com>
2025-06-24 11:41:54 +0530

    Add files via upload
    Typescript notes

---
+++ React,Typescript/1.4.25.ts
@@ -0,0 +1,58 @@
+let email = 'shiva.teja@gmail.com';
+
+// email = 'akash@gmail.com';
+
+console.log(email);
+
+// const isUserLoggedIn: boolean = true;
+
+// const score: number = 16;
+
+// const meetings: Array<string> = ['Python-2508 12:30 PM', 'Python-2503 11 AM', ' React-2503 9:30 AM', 'React -2501- 8 AM'];
+
+// console.log(meetings);
+
+// interface Meeting {
+//     name: string;
+//     type: string;
+//     id: string;
+//     passcode: number;
+//     getPasscode(): string;
+// }
+
+// const meetingsA: Array<Meeting> = [
+//     {
+//         name: 'python-2503',
+//         type: 'Recurring',
+//         id: '878 6979 0995',
+//         passcode: 300339,
+//         getPasscode: (): string => {
+//             return `Passcode: ${this.passcode}`
+        // }
+    // },
+    // {
+    //     name: 'Python-2508 12:30 PM',
+    //     type: 'Recurring',
+    //     id: '856 8108 0074',
+    //     passcode: 300340,
+    //     getPasscode: (): string => {
+    //         return `Passcode: ${this.passcode}`
+    //     }
+    // },
+    // {
+    //     name: 'Python-2503 11 AM',
+    //     type: 'Recurring',
+    //     id: '870 2780 2689',
+    //     passcode: 300341,
+//         getPasscode: (): string => {
+//             return `Passcode: ${this.passcode}`
+
+//         }
+//     }
+// ];
+
+// console.log(meetingsA[0]);
+
+// let id: any = 'DE001';
+
+// id = 1;
